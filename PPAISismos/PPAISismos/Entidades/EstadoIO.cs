0ad65359a79f2eec08ab39486e4973977165b6a8
emili <emiliogalimberti@gmail.com>
2025-05-14 19:48:02 -0300

    Add project files.

---
+++ PPAISismos/PPAISismos/Entidades/EstadoIO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAISismos.Entidades
+{
+    public class EstadoIO
+    {
+        private string nombre;
+        
+        public EstadoIO(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public bool esRealizada()
+        { 
+            return nombre.Equals("Realizada", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+    }
+}

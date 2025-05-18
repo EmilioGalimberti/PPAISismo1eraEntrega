50aba19f1a92e7477bcfc1a0e6883e2b965ff99c
emili <emiliogalimberty@gmail.com>
2025-05-17 23:45:05 -0300

    +Usuario

---
+++ PPAISismos/PPAISismos/Entidades/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAISismos.Entidades
+{
+    public class Usuario
+    {
+        private string nombreUsuario { get; set; }
+        private string contrasena { get; set; }
+        private Empleado empleado { get; set; }
+
+        public Usuario(string nombreUsuario, string contrasena, Empleado empleado)
+        {
+            this.nombreUsuario = nombreUsuario;
+            this.contrasena = contrasena;
+            this.empleado = empleado;
+        }
+    }
+}

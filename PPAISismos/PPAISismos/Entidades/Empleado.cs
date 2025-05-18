3a5e745fec98929e34024d9f16bba0e39b712a09
emili <emiliogalimberty@gmail.com>
2025-05-17 23:33:01 -0300

    +Correciones en el constructor Empleado

--- PPAISismos/PPAISismos/Entidades/Empleado.cs
+++ PPAISismos/PPAISismos/Entidades/Empleado.cs
@@ -9,19 +9,16 @@ namespace PPAISismos.Entidades
         private string apellido { get; set; }
         private string mail { get; set; }
         private int telefono { get; set; }
-        private string email { get; set; }
-
         //un cliente tiene un rol
         private Rol rol { get; set; }
 
         //Constructor
-        public Empleado(string nombre, string apellido, string dni,  int telefono, string email, Rol rol)
+        public Empleado(string nombre, string apellido, string mail,  int telefono,  Rol rol)
         {
             this.nombre = nombre;
             this.apellido = apellido;
-            this.mail = dni;
             this.telefono = telefono;
-            this.email = email;
+            this.mail = mail;
             this.rol = rol;
         }
         //Getters y Setters AGREGARLOS EN CASO DE SER UN METODO ESPECIFICO QUE SE USE EN EL DIAGRAMA DE SECUENCIA

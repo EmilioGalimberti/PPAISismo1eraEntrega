efeaee5f080a42657d172133ce495f22ff6960f3
emili <emiliogalimberty@gmail.com>
2025-05-17 23:19:14 -0300

    +Rol +Empleado ->1 Rol

--- PPAISismos/PPAISismos/Entidades/Empleado.cs
+++ PPAISismos/PPAISismos/Entidades/Empleado.cs
@@ -1,4 +1,6 @@
-﻿namespace PPAISismos.Entidades
+﻿using System.Windows.Forms;
+
+namespace PPAISismos.Entidades
 {
     public class Empleado
     {
@@ -8,14 +10,19 @@
         private string mail { get; set; }
         private int telefono { get; set; }
         private string email { get; set; }
+
+        //un cliente tiene un rol
+        private Rol rol { get; set; }
+
         //Constructor
-        public Empleado(string nombre, string apellido, string dni,  int telefono, string email)
+        public Empleado(string nombre, string apellido, string dni,  int telefono, string email, Rol rol)
         {
             this.nombre = nombre;
             this.apellido = apellido;
             this.mail = dni;
             this.telefono = telefono;
             this.email = email;
+            this.rol = rol;
         }
         //Getters y Setters AGREGARLOS EN CASO DE SER UN METODO ESPECIFICO QUE SE USE EN EL DIAGRAMA DE SECUENCIA
  
---
+++ PPAISismos/PPAISismos/Entidades/Rol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAISismos.Entidades
+{
+    public class Rol
+    {
+        private string nombre { get; set; }
+        private string descripcionRol { get; set; }
+
+        public Rol(string nombre, string descripcionRol)
+        {
+            this.nombre = nombre;
+            this.descripcionRol = descripcionRol;
+        }
+    }
+}

f010c4174b24971971e93bb67bae016453af927b
emili <emiliogalimberty@gmail.com>
2025-05-18 01:57:52 -0300

    +EstacionSismologica +OI

---
+++ PPAISismos/PPAISismos/Entidades/EstacionSismologica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAISismos.Entidades
+{
+    public class EstacionSismologica
+    {
+        //Atributos
+        //VERIFICAR LOS TIPOS DE ATRIBUTOS
+        private int codigoEstacion;
+        private bool documentoCerificacionAdquirido;
+        private int latitud;
+        private int longitud;
+        private string nombre;
+        private int nroCerficicacionAdquisicion;
+
+        public EstacionSismologica(int codigoEstacion, bool documentoCerificacionAdquirido, int latitud, int longitud, string nombre, int nroCerficicacionAdquisicion)
+        {
+            this.codigoEstacion = codigoEstacion;
+            this.documentoCerificacionAdquirido = documentoCerificacionAdquirido;
+            this.latitud = latitud;
+            this.longitud = longitud;
+            this.nombre = nombre;
+            this.nroCerficicacionAdquisicion = nroCerficicacionAdquisicion;
+        }
+
+    }
+}
---
+++ PPAISismos/PPAISismos/Entidades/OrdenDeInspeccion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAISismos.Entidades
+{
+    public class OrdenDeInspeccion
+    {
+        //Atributos
+        //OI->1 Empleado
+        private Empleado empleado;
+        //OI->1 EstacionSimologica
+        private EstacionSismologica estacionSismologica;
+        //OI->1 EstadoOI
+        private EstadoOI estadoOI;
+
+        private DateTime fechaHoraCierre;
+        private DateTime fechaHoraInicio;
+        private DateTime fechaHoraFinalizacion;
+        private int numeroOrden;
+        private string observacionCierre;
+
+        //Constructor
+        public OrdenDeInspeccion(Empleado empleado,EstacionSismologica estacionSismologica,EstadoOI estadoOI,DateTime fechaHoraCierre,DateTime fechaHoraInicio,DateTime fechaHoraRealizacion,int numeroOrden,string observacionCierre)
+        {
+            this.empleado = empleado;
+            this.estacionSismologica = estacionSismologica;
+            this.estadoOI = estadoOI;
+            this.fechaHoraCierre = fechaHoraCierre;
+            this.fechaHoraInicio = fechaHoraInicio;
+            this.fechaHoraFinalizacion = fechaHoraRealizacion;
+            this.numeroOrden = numeroOrden;
+            this.observacionCierre = observacionCierre;
+        }
+    }
+}
